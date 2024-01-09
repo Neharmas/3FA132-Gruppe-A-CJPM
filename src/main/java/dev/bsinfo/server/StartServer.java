4ecae3c6215fa98472a73da94f1a7ce86c6bb548
Christopher Ajomale <Christopher.Ajomale@ub.uni-muenchen.de>
2024-01-09 11:32:02 +0100

    made Server run method return a bool

--- src/main/java/dev/bsinfo/server/StartServer.java
+++ src/main/java/dev/bsinfo/server/StartServer.java
@@ -3,6 +3,7 @@ package dev.bsinfo.server;
 import java.net.URI;
 
 import dev.hv.db.init.DBConnect;
+import jakarta.ws.rs.ProcessingException;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import com.sun.net.httpserver.HttpServer;
@@ -12,7 +13,7 @@ public class StartServer {
     final String pack = "dev.bsinfo.ressource";
     String url = "http://localhost:8080/rest";
     private StartServer() {}
-
+    
     public static StartServer getInstance()
     {
         if (instance == null)
@@ -22,12 +23,20 @@ public class StartServer {
         return instance;
     }
 
-    public void run()
+    public boolean run()
     {
         System.out.println("Start server");
         System.out.println(url);
         final ResourceConfig rc = new ResourceConfig().packages(pack);
-        final HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create(url), rc);
-        System.out.println("Ready for Requests....");
+        try
+        {
+            final HttpServer server = JdkHttpServerFactory.createHttpServer(URI.create(url), rc);
+            System.out.println("Ready for Requests....");
+            return true;
+        }
+        catch (ProcessingException e) {
+            System.out.println("Server couldn't be initialized.");
+        }
+        return false;
     }
 }
