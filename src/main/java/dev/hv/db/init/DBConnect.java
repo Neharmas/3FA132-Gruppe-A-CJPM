9654bc4f5044d77d526ae22ab265e84a8041fd21
ProxySinged <paul_maile@web.de>
2024-03-06 10:12:19 +0100

    feature & refactor: add method to read table and refactor create table methods for future feature of importing single tables

--- src/main/java/dev/hv/db/init/DBConnect.java
+++ src/main/java/dev/hv/db/init/DBConnect.java
@@ -11,6 +11,8 @@ import java.util.Properties;
 
 import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class DBConnect implements IDbConnect{
 	private static DBConnect instance = null;
@@ -30,34 +32,12 @@ public class DBConnect implements IDbConnect{
 
 	@Override
 	public void createAllTables() {		
-		final Handle handle = jdbi.open();
-		//create table KUNDEN
-		handle.execute("CREATE TABLE IF NOT EXISTS Customer ("
-				+ "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-				+ "lastname TEXT NOT NULL,"
-				+ "firstname TEXT NOT NULL);");
-		//create table Nutzer
-		handle.execute("CREATE TABLE IF NOT EXISTS User ("
-				+ "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-				+ "lastname TEXT NOT NULL,"
-				+ "firstname TEXT NOT NULL,"
-				+ "token TEXT NOT NULL,"
-				+ "password TEXT NOT NULL);");
-		//create table Lesen
-		handle.execute("CREATE TABLE IF NOT EXISTS Reading ("
-				+ "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-				+ "comment TEXT NOT NULL,"
-				+ "customer INTEGER NOT NULL,"
-				+ "dateofreading TEXT NOT NULL,"
-				+ "kindofmeter TEXT NOT NULL,"
-				+ "meterid TEXT NOT NULL,"
-				+ "metercount INTEGER NOT NULL,"
-				+ "substitute BOOLEAN NOT NULL,"
-				+ "FOREIGN KEY (customer) REFERENCES Customer (id)"
-				+ ");");
-		
-		handle.close();
+		createCustomerTable();
+		createUserTable();
+		createReadingTable();
 	}
+	
+	
 	
 	public void insertTestData() {
 		final Handle handle = jdbi.open();
@@ -126,13 +106,56 @@ public class DBConnect implements IDbConnect{
 	}
 
 	private String determineColumnType(Object value) {
-		// Used for Inports
+		// Used for Imports
 		switch(getValueType(value)) {
 			case "Float": return "FLOAT";
 			case "Integer": return "INT";
 			case "Boolean": return "BOOL";
 			default: return "TEXT";
 		}
+	}
+	
+	public void createCustomerTable() {
+		final Handle handle = jdbi.open();
+		
+		handle.execute("CREATE TABLE IF NOT EXISTS Customer ("
+				+ "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+				+ "lastname TEXT NOT NULL,"
+				+ "firstname TEXT NOT NULL);");
+
+		handle.close();
+	}
+	
+	public void createUserTable() {
+		final Handle handle = jdbi.open();
+		
+		handle.execute("CREATE TABLE IF NOT EXISTS User ("
+				+ "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+				+ "lastname TEXT NOT NULL,"
+				+ "firstname TEXT NOT NULL,"
+				+ "token TEXT NOT NULL,"
+				+ "password TEXT NOT NULL);");
+
+		handle.close();
+	}
+	
+	public void createReadingTable() {
+		final Handle handle = jdbi.open();
+		
+		handle.execute("CREATE TABLE IF NOT EXISTS Reading ("
+				+ "id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+				+ "comment TEXT NOT NULL,"
+				+ "customer INTEGER NOT NULL,"
+				+ "dateofreading TEXT NOT NULL,"
+				+ "kindofmeter TEXT NOT NULL,"
+				+ "meterid TEXT NOT NULL,"
+				+ "metercount INTEGER NOT NULL,"
+				+ "substitute BOOLEAN NOT NULL,"
+				//dependent on existing customer table
+				+ "FOREIGN KEY (customer) REFERENCES Customer (id)" 
+				+ ");");
+
+		handle.close();
 	}
 	
 	private String buildColumnsSpecification(String[] columnNames, String[] columnType, String[] constrains) {
@@ -156,12 +179,47 @@ public class DBConnect implements IDbConnect{
 
 	public List<Map<String, Object>> readTable(String tablename) {
 		final Handle handle = jdbi.open();
-		List<Map<String, Object>> res = handle.select("SELECT * FROM TABLE " + tablename + " ;").mapToMap().list();
+		List<Map<String, Object>> res = handle.select("SELECT * FROM " + tablename + ";").mapToMap().list();
 		handle.close();
 		
 		return res;
 	}
 	
+	public String readTableAsMap(String tablename) {
+		final Handle handle = jdbi.open();
+		//Map<String, Object> res = handle.select("SELECT * FROM " + tablename + ";").toString();
+		String res = handle.select("SELECT * FROM " + tablename + ";").toString();
+		handle.close();
+		
+		return res;
+	}
 	
+	public static void main(String[] args) {
+		DBConnect db = new DBConnect();
+		Jdbi jdbi = db.getJdbi();
+		db.createAllTables();
+		db.insertTestData();
+
+		List<Map<String, Object>> customers = db.readTable("Customer");
+		//List<Map<String, Object>> users = db.readTable("User");
+		//List<Map<String, Object>> readings = db.readTable("Reading");
+		//List<Map<String, Object>> fictional = db.readTable("fictional");
+		//System.out.println(customers);
+		System.out.println(customers);
+		//String customerCutted = customers.toString().substring(1, customers.toString().length() - 1);
+		
+		String customerWithKey = "{Customer: " + customers;
+
+		
+		JSONObject json = new JSONObject(customerWithKey);
+		
+		System.out.println(json);
+		/** [{id=1, lastname=ln1, firstname=fn1}, {id=3, lastname=Baggins, firstname=Frodo}, {id=4, lastname=Baggins, firstname=Frodo}, {id=5, lastname=Baggins, firstname=Frodo}, {id=6, lastname=Baggins, firstname=Frodo}, {id=7, lastname=Hasan, firstname=Mouawia}, {id=8, lastname=Maile, firstname=Paul}, {id=9, lastname=Mandl, firstname=Julian}, {id=10, lastname=Ajomale, firstname=Christopher}, {id=11, lastname=Hasan, firstname=Mouawia}, {id=12, lastname=Maile, firstname=Paul}, {id=13, lastname=Mandl, firstname=Julian}, {id=14, lastname=Ajomale, firstname=Christopher}, {id=15, lastname=Hasan, firstname=Mouawia}, {id=16, lastname=Maile, firstname=Paul}, {id=17, lastname=Mandl, firstname=Julian}, {id=18, lastname=Ajomale, firstname=Christopher}, {id=19, lastname=Hasan, firstname=Mouawia}, {id=20, lastname=Maile, firstname=Paul}, {id=21, lastname=Mandl, firstname=Julian}, {id=22, lastname=Ajomale, firstname=Christopher}, {id=23, lastname=Hasan, firstname=Mouawia}, {id=24, lastname=Maile, firstname=Paul}, {id=25, lastname=Mandl, firstname=Julian}, {id=26, lastname=Ajomale, firstname=Christopher}, {id=27, lastname=Hasan, firstname=Mouawia}, {id=28, lastname=Maile, firstname=Paul}, {id=29, lastname=Mandl, firstname=Julian}, {id=30, lastname=Ajomale, firstname=Christopher}, {id=31, lastname=Hasan, firstname=Mouawia}, {id=32, lastname=Maile, firstname=Paul}, {id=33, lastname=Mandl, firstname=Julian}, {id=34, lastname=Ajomale, firstname=Christopher}, {id=35, lastname=Hasan, firstname=Mouawia}, {id=36, lastname=Maile, firstname=Paul}, {id=37, lastname=Mandl, firstname=Julian}, {id=38, lastname=Ajomale, firstname=Christopher}, {id=39, lastname=Hasan, firstname=Mouawia}, {id=40, lastname=Maile, firstname=Paul}, {id=41, lastname=Mandl, firstname=Julian}, {id=42, lastname=Ajomale, firstname=Christopher}]
+			[{id=1, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=2, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=3, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=4, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=5, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=6, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=7, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=8, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=9, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=10, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=11, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=12, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=13, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=14, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=15, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=16, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=17, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=18, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=19, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=20, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=21, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=22, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=23, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=24, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=25, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=26, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=27, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=28, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=29, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=30, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=31, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=32, lastname=Moses, firstname=Hannes, token=777, password=100}, {id=33, lastname=Fittler, firstname=Arnold, token=12345, password=123456}, {id=34, lastname=Franziska, firstname=Anne, token=88420, password=66699}, {id=35, lastname=Gandhi, firstname=Mahatma, token=112, password=42}, {id=36, lastname=Moses, firstname=Hannes, token=777, password=100}]
+			[{id=1, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=2, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=3, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=4, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=5, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=6, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=7, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=8, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=9, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=10, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=11, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=12, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=13, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=14, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=15, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=16, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}, {id=17, comment=test1, customer=1, dateofreading=12345, kindofmeter=pla, meterid=222, metercount=22ss, substitute=0}, {id=18, comment=test2, customer=2, dateofreading=8888, kindofmeter=dlp, meterid=234, metercount=1s1, substitute=1}] **/
+		//System.out.println(fictional);
+		
+		
+	}
 	
 }
